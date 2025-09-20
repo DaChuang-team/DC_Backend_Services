305aec7efbb05cb18e064970d59790f71c6ee5b1
Marguerite68 <sekaibest@outlook.com>
2025-09-20 11:04:54 +0800

    First commit

---
+++ src/main/java/org/dachuang_team/dc_backend_services/DcBackendServicesApplication.java
@@ -0,0 +1,13 @@
+package org.dachuang_team.dc_backend_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DcBackendServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DcBackendServicesApplication.class, args);
+	}
+
+}
