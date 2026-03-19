e00423d0b02e96f99bec16e40912b85ac175ec4e
Marguerite68 <sekaibest@outlook.com>
2026-03-19 19:48:50 +0800

    新增AI交互图片和商品冗余图片清理任务，优化图片管理逻辑。目前有OSS删除权限问题待解决。

--- src/main/java/org/dachuang_team/dc_backend_services/DcBackendServicesApplication.java
+++ src/main/java/org/dachuang_team/dc_backend_services/DcBackendServicesApplication.java
@@ -2,8 +2,10 @@ package org.dachuang_team.dc_backend_services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DcBackendServicesApplication {
 
     public static void main(String[] args) {
