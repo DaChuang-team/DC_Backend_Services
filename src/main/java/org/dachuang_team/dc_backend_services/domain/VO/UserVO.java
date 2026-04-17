1f1830281c97e6014c0565ed656837c7c89b9e0d
Marguerite68 <sekaibest@outlook.com>
2026-04-17 16:16:24 +0800

    新增用户手机号通过验证码换绑功能，修复了一些已知问题

--- src/main/java/org/dachuang_team/dc_backend_services/domain/VO/UserVO.java
+++ src/main/java/org/dachuang_team/dc_backend_services/domain/VO/UserVO.java
@@ -1,57 +1,61 @@
-package org.dachuang_team.dc_backend_services.domain.DTO;
+package org.dachuang_team.dc_backend_services.domain.VO;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-
-
-public class UserUpdateDTO {
-    @JsonProperty("newName")//前端传入的新用户名
+public class UserVO {
     private String userName;
-    @JsonProperty("userPreference")
+    private String userPhone;
+    private char userGender;
     private String userPreference;
-    @JsonProperty("userGender")
-    private Character userGender;
-    @JsonProperty("userAvatarURL")
     private String userAvatarURL;
-    @JsonProperty("userBirthday")
     private String userBirthday;
+    private String userStatus;
+    private Integer points;
 
+    public String getUserStatus() {
+        return userStatus;
+    }
+    public void setUserStatus(String userStatus) {
+        this.userStatus = userStatus;
+    }
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
-
+    public String getUserPhone() {
+        return userPhone;
+    }
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone;
+    }
     public String getUserPreference() {
         return userPreference;
     }
-
     public void setUserPreference(String userPreference) {
         this.userPreference = userPreference;
     }
-
-    public Character getUserGender() {
-        return userGender;
-    }
-
-    public void setUserGender(Character userGender) {
-        this.userGender = userGender;
-    }
-
     public String getUserAvatarURL() {
         return userAvatarURL;
     }
-
     public void setUserAvatarURL(String userAvatarURL) {
         this.userAvatarURL = userAvatarURL;
     }
-
     public String getUserBirthday() {
         return userBirthday;
     }
-
     public void setUserBirthday(String userBirthday) {
         this.userBirthday = userBirthday;
+    }
+    public Integer getPoints() {
+        return points;
+    }
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+    public char getUserGender() {
+        return userGender;
+    }
+    public void setUserGender(char userGender) {
+        this.userGender = userGender;
     }
 }
